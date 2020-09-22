b571032c56dc285ebb8b8f8df1f0fe4d5be613e0
Nicholas Nelson <nelsonni@oregonstate.edu>
2020-09-22 14:01:55 -0700

    Redirected tests towards EPICLab urls

--- tests/test_github.py
+++ tests/test_github.py
@@ -10,10 +10,10 @@ class GitHubTest(unittest.TestCase):
 		self.g = None
 
 	def test_repo_exists(self):
-		self.assertTrue(self.g.repoExists("caiusb", "github-utils"))
+		self.assertTrue(self.g.repoExists("EPICLab", "EPICLab.github.io"))
 
 	def test_repo_doesnt_exit(self):
-		self.assertFalse(self.g.repoExists("caiusb", "invalid"))
+		self.assertFalse(self.g.repoExists("EPICLab", "invalid"))
 
 	def test_rate_limit(self):
 		if (self.g.usesAuth()):
@@ -27,21 +27,21 @@ class GitHubTest(unittest.TestCase):
 		self.assertTrue(reposNo >= 47)
 
 	def test_headers(self):
-		stargazers = self.g.get('/repos/caiusb/gitective/stargazers', headers={'Accept': 'application/vnd.github.v3.star+json'})
+		stargazers = self.g.get('/repos/EPICLab/miner-utils/stargazers', headers={'Accept': 'application/vnd.github.v3.star+json'})
 		self.assertTrue(len(stargazers) > 0)
 		self.assertTrue(len(stargazers[0].keys()) == 2)
 
 	def test_follow_links(self):
-		repo = self.g.get('/repos/caiusb/gitective')
+		repo = self.g.get('/repos/caiusb/miner-utils')
 		forks = self.g.get(repo['forks_url'])
 		self.assertTrue(len(forks) > 0)
 
 	def test_search_api(self):
-		users = self.g.get('/search/users', params={'q': 'caiusb'})
+		users = self.g.get('/search/users', params={'q': 'EPICLab'})
 		self.assertTrue(len(users) > 0)
 
 	def test_url_params(self):
-		users = self.g.get('/search/users?q=caiusb')
+		users = self.g.get('/search/users?q=EPICLab')
 		self.assertTrue(len(users) > 0)
 
 if __name__ == '__main__':
