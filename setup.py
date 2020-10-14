802d614ea39eb6f35797d2033717f1b953a6825f
Nicholas Nelson <nelsonni@oregonstate.edu>
2020-10-14 14:32:54 -0700

    Updated install to use setup.cfg format for setuptools v30.3.0

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python
-
 import setuptools
-from distutils.core import setup
-
-setup(name='MinerUtils',
-      version='2.6.0',
-      description='GitHub API Access Utilities',
-      author='Nicholas Nelson & Caius Brindescu',
-      author_email='nelsonni@oregonstate.edu',
-      url='https://github.com/EPICLAB/miner-utils',
-      packages=['minerutils'],
-      install_requires=['requests']
-     )
+setuptools.setup()
